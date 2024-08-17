8f7924b6c44dc03149aff70e7a71619145dca037
vuhieuminh2597 <vuhieuminh2597@gmail.com>
2024-08-17 15:01:39 +0700

    update 0.0.2

--- Learn DSAA/src/array1way/ex2.java
+++ Learn DSAA/src/array1way/ex2.java
@@ -2,11 +2,7 @@ package array1way;
 
 import java.util.Scanner;
 
-public class ex1 {
-    /**
-     * Ex1: Liệt kê tất cả các số nguyên tố trong mảng.
-     *
-     */
+public class ex2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int t = 1;
@@ -18,8 +14,6 @@ public class ex1 {
             System.out.println("Số phần tử mảng:");
             n = input.nextInt();
             number = addArray(n, input);
-            showArray(number);
-            showIsPrime(number);
             t--;
         }
     }
@@ -42,27 +36,5 @@ public class ex1 {
         System.out.println();
     }
 
-    public static boolean checkIsPrime(int n){
 
-        if (n < 2){
-            return false;
-        } else {
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0){
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-
-    public static void showIsPrime(int[] n){
-        System.out.println("Số nguyên tố:");
-        for (int i = 0; i < n.length; i++) {
-            if (checkIsPrime(n[i])){
-                System.out.print(n[i] + " ");
-            }
-        }
-        System.out.println();
-    }
 }
